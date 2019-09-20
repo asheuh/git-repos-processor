9b56ebd3b06ee91676c0ac4e7846811c7e39f800
asheuh <asheuh49@gmail.com>
2019-09-21 00:28:56 +0300

    use threading

--- src/clone.py
+++ src/clone.py
@@ -1,20 +1,26 @@
-#! /usr/bin/env python3
+#!/usr/bin/env python3.7
 """
 imports
 """
+# external modules
 import subprocess
 import os
+import click
 import time
 import concurrent.futures
-import pdb
-import click
-from functools import reduce
+
+from multiprocessing import Pool
+from functools import lru_cache
+from typing import Generator
+
+# internal modules
+from repo_stats import RepoStatistics
 
 @click.group()
 def cli():
     pass
 
-def repo_generator(array):
+def repo_generator(array: list) -> Generator[str, None, None]:
     """
     generator implementation to help clone repo 
     one by one
@@ -23,13 +29,11 @@ def repo_generator(array):
         yield f'git clone {item.decode("utf-8")}'
 
 
-def generate_gclone_links(filename):
+def generate_gclone_links(filename: str) -> list:
     """
     clone repos from a specified txt file
-    : an alternative code
-    repo_array = list(map(lambda repo: 'git clone ' + repo.decode("utf-8"), content))
-    iterator = iter(repo_array)
-    reduce(lambda _, b: clone_it(f'git clone {b}'), map(lambda repo: repo.decode("utf-8"), content))
+    utilize python generators to create an
+    iterator that has list of all repos
     """
     if os.path.exists(filename):
         with open(filename, 'rb') as f:
@@ -37,12 +41,13 @@ def generate_gclone_links(filename):
                 content = f.readlines()
                 generator = repo_generator(content) # creating and initializing a python generator
                 repos = list(generator)
+                repos.pop(0)
                 return repos
             except Exception as e:
                 print(e)
 
 
-def clone_it(command):
+def process_command(command):
     process = subprocess.Popen(command, stdout=subprocess.PIPE, shell=True)
     output, error = process.communicate()
 
@@ -53,37 +58,24 @@ def clone_it(command):
     return None
 
 
-@click.command('user')
-@click.argument('username')
-def clone_user_repos(username):
-    """
-    This script clones repos for a specified user
-    """
-    bash_commands = (
-            f"""
-            brew install jq;
-            mkdir cloned-repos;
-            cd cloned-repos && \
-                    UserName={username}; curl -s \
-                    https://api.github.com/users/$UserName/repos\?per_page\=1000 | \
-                    jq -r '.[]|.clone_url' | xargs -L1 git clone
-                    """)
-    clone_it(bash_commands)
-
-
 @click.command('file')
 @click.argument('filename')
-def run_command(filename):
-    with concurrent.futures.ThreadPoolExecutor() as executor:
-        executor.map(clone_it, generate_gclone_links(filename))
+@click.argument('number_of_threads')
+@lru_cache(maxsize=128, typed=False)
+def run_command(filename, number_of_threads):
+    t1 = time.perf_counter()
+    with concurrent.futures.ThreadPoolExecutor(int(number_of_threads)) as executor:
+        executor.map(process_command, generate_gclone_links(filename))
+    t2 = time.perf_counter()
 
-
-def add_commands():
-    """Add command to groups"""
-    cli.add_command(run_command)
-    cli.add_command(clone_user_repos)
+    print(f"Finished cloning in {t2-t1} second(s)")
 
 
 if __name__ == "__main__":
-    add_commands()
+    cli.add_command(run_command)
     cli()
+
+
+
+
+
---
+++ src/repo_stats.py
@@ -0,0 +1,38 @@
+"""
+imports
+"""
+
+class RepoStatistics:
+    """
+    Computes certain statistics only for python code present
+    in each github repository
+    """
+    def __init__(self, repository):
+        self.repository = repository
+
+    @classmethod
+    def line_of_code(cls):
+        """
+        to compute the number of lines of python
+        code used in the repository
+        (excludes comments, whitespaces, blank lines)
+        """
+        return None
+
+    @classmethod
+    def external_lib_pkg(cls):
+        """
+        finds all the libraries/packages used in
+        the repository and stores them in a list data
+        structure
+        """
+        return None
+
+    @classmethod
+    def nesting_factor(cls):
+        """
+        Computes the nesting factor for the repository
+        :this is the average depth of a nested for loop
+        throughout the code
+        """
+        return None
