435f66713993baa8e43eceaf871c50b89f30dbef
asheuh <asheuh49@gmail.com>
2019-09-23 16:50:32 +0300

    compute number of lines in python files for repo

--- src/process_repos.py
+++ src/process_repos.py
@@ -1,6 +1,9 @@
 """
 imports
 """
+import os
+import time
+
 
 class RepoStatistics:
     """
@@ -10,17 +13,29 @@ class RepoStatistics:
     def __init__(self, repository):
         self.repository = repository
 
-    @classmethod
-    def lines_of_code(cls):
+    def lines_of_code(self):
         """
         to compute the number of lines of python
         code used in the repository
         (excludes comments, whitespaces, blank lines)
+        TODO: Optimize this method to consider complexity
         """
-        return None
+        ignore_dir = ['.git', '.github']
+        total = 0
+        if os.path.isdir(self.repository):
+            for dir_name, subdir, files in os.walk(self.repository):
+                subdir[:] = [d for d in subdir if d not in ignore_dir]
+                for filename in files:
+                    if filename.endswith('.py'):
+                        with open(os.path.join(dir_name, filename), 'rb') as f:
+                            content = [
+                                    line for line in f.readlines()
+                                    if not line.startswith(b'#')
+                                    ]
+                            total += len(content)
+        return total
 
-    @classmethod
-    def external_lib_pkg(cls):
+    def external_lib_pkg(self):
         """
         finds all the libraries/packages used in
         the repository and stores them in a list data
@@ -28,11 +43,22 @@ class RepoStatistics:
         """
         return None
 
-    @classmethod
-    def nesting_factor(cls):
+    def nesting_factor(self):
         """
         Computes the nesting factor for the repository
         :this is the average depth of a nested for loop
         throughout the code
         """
         return None
+
+
+if __name__ == "__main__":
+    start = time.time()
+    REPO = RepoStatistics("Multitask-and-Transfer-Learning")
+    REPO.lines_of_code()
+    elapsed = time.time() - start
+
+    print(f'Time taken: {elapsed} seconds')
+
+
+
